10e649bf5a3e1c46b45bf715e9517b89fa261edf
Albert G <albert.gafiyatullin@outlook.com>
2022-08-28 17:01:19 +0700

    gc moved to coolc/docs

--- gc/src/gc/gc-impl/globals.cpp
+++
@@ -1,9 +0,0 @@
-#include "gc/gc-interface/alloca.hpp"
-#include "gc/gc-interface/gc.hpp"
-#include "gc/gc-interface/mark.hpp"
-
-bool LOGSWEEP = false;
-bool LOGGING = false;
-bool LOGMARK = false;
-bool LOGALOC = false;
-bool ZEROING = false;
--- gc/src/workload/workloads.hpp
+++
@@ -1,6 +0,0 @@
-#include "linked-list-workloads.hpp"
-#include "sanity-workloads.hpp"
-
-// simple tests
-void sanity_workload();
-void linked_list_workload();
