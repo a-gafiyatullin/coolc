00d851b681d69ffeec810937be72cee740037838
Albert Gafiyatullin <albert.gafiyatullin@outlook.com>
2024-02-27 05:22:30 +0000

    Enable GC testing.

--- tests/codegen/make_results.sh
+++ tests/codegen/make_results.sh
@@ -15,7 +15,7 @@ cd tests/
 for file in *.cl; do
     filename=${file%.*}
     $1/coolc $file -o $TEST_DIR/out/$filename
-    $2 $1 $TEST_DIR/tests/ $TEST_DIR/results/$file.result $TEST_DIR/out/ $filename
+    $2 $1 $TEST_DIR/tests/ $TEST_DIR/results/$file.result $TEST_DIR/out/ $filename $3 $4
 done;
 
 cd $CURR_DIR
